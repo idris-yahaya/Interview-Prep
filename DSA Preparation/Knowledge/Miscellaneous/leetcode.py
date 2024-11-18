9f7ef75c97ed2da96f35c9d722f30bf52d7818b3
Idris Yahaya <idrisy2237@gmail.com>
2024-11-18 05:13:42 -0500

    Add Solving Techniques to LeetCode Problems

--- DSA Preparation/Knowledge/Miscellaneous/leetcode.py
+++ DSA Preparation/Knowledge/Miscellaneous/leetcode.py
@@ -1,13 +1,13 @@
 """ [Problem Statement] """
-# [LeetCode Link]
-# [NeetCode Link]
+# LeetCode: [Link]
+# NeetCode: [Link]
 
 """ Solution: [Approach] """
 # Time Complexity: [Big-O]
 # Space Complexity: [Big-O]
 
 # Thought Process:
-# [Insert explanation of approach and reasoning]
+#  - [Insert explanation of approach and reasoning]
 
 # Code Solution
 class Solution:
@@ -18,21 +18,38 @@ class Solution:
 """ Coding Workflow """
 
 # 1. Understand (5 mins)
-# - Rephrase the problem to confirm understanding
-# - Clarify constraints, assumptions, and edge cases
-# - Define input and output specifications and formats
-# - Validate understanding with a simplified example
+#  - Rephrase the problem to confirm understanding
+#  - Clarify constraints, assumptions, and edge cases
+#  - Define input and output specifications and formats
+#  - Validate understanding with a simplified example
 
 # 2. Strategize (5 mins)
-# - Discuss potential approaches (trade-offs, complexities)
-# - Agree on the best approach; outline steps and optimize
+#  - Discuss potential approaches (trade-offs, complexities)
+#  - Agree on the best approach; outline steps and optimize
 
 # 3. Implement (15 mins)
-# - Write clean, modular code while explaining your reasoning
-# - Utilize meaningful comments, variables, and function names
-# - Request permission for shortcuts or stubbing trivial functions
+#  - Write clean, modular code while explaining your reasoning
+#  - Utilize meaningful comments, variables, and function names
+#  - Request permission for shortcuts or stubbing trivial functions
 
 # 4. Review (5 mins)
-# - Scan for errors and identify potential refactoring
-# - Test with edge cases and the provided examples
-# - Reiterate complexities and suggest improvements
+#  - Scan for errors and identify potential refactoring
+#  - Test with edge cases and the provided examples
+#  - Reiterate complexities and suggest improvements
+
+
+""" Solving Techniques """
+
+# Problem Approaches
+# - Visualize the Problem
+# - Solve Manually First
+# - Generate More Examples
+# - Break Problems Down
+# - Apply Common DSA’s
+
+# Time & Space Optimization
+#  - Recognize BTTC
+#  - Reuse Computations
+#  - Eliminate Redundancy
+#  - In-Place Modifications
+#  - Experiment With DSA’s

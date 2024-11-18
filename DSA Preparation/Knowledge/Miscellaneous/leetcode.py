6ed9d5b39cf29bc6d9f574737f6f9401b0e3481a
Idris Yahaya <idrisy2237@gmail.com>
2024-11-18 04:47:03 -0500

    Add Workflow for Interview Questions

---
+++ DSA Preparation/Knowledge/Miscellaneous/leetcode.py
@@ -0,0 +1,38 @@
+""" [Problem Statement] """
+# [LeetCode Link]
+# [NeetCode Link]
+
+""" Solution: [Approach] """
+# Time Complexity: [Big-O]
+# Space Complexity: [Big-O]
+
+# Thought Process:
+# [Insert explanation of approach and reasoning]
+
+# Code Solution
+class Solution:
+    def solution(self):
+        pass
+
+
+""" Coding Workflow """
+
+# 1. Understand (5 mins)
+# - Rephrase the problem to confirm understanding
+# - Clarify constraints, assumptions, and edge cases
+# - Define input and output specifications and formats
+# - Validate understanding with a simplified example
+
+# 2. Strategize (5 mins)
+# - Discuss potential approaches (trade-offs, complexities)
+# - Agree on the best approach; outline steps and optimize
+
+# 3. Implement (15 mins)
+# - Write clean, modular code while explaining your reasoning
+# - Utilize meaningful comments, variables, and function names
+# - Request permission for shortcuts or stubbing trivial functions
+
+# 4. Review (5 mins)
+# - Scan for errors and identify potential refactoring
+# - Test with edge cases and the provided examples
+# - Reiterate complexities and suggest improvements

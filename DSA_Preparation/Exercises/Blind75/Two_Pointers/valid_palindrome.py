2fa8ee3d61563bc6c87af986cfa77c61f1ed5ac1
Idris Yahaya <idrisy2237@gmail.com>
2025-01-05 23:44:18 -0500

    Solve Valid Palindrome

---
+++ DSA_Preparation/Exercises/Blind75/Two_Pointers/valid_palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left_c, right_c = 0, len(s) - 1
+        alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+        s = "".join(s.split())
+        s = s.lower()
+
+        while left_c < right_c:
+            # Move left_c if it's not pointing to an alphanumeric character
+            if s[left_c] not in alphanumeric:
+                left_c += 1
+                continue
+            
+            # Move right_c if it's not pointing to an alphanumeric character
+            if s[right_c] not in alphanumeric:
+                right_c -= 1
+                continue
+            
+            # Compare characters after ensuring both are alphanumeric
+            if s[left_c] != s[right_c]:
+                return False
+            
+            # Move pointers inward
+            left_c += 1
+            right_c -= 1
+        
+        return True
